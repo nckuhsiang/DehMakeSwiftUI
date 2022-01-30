cc058d387e9df99b6d3eec8a71ac7d7886ccf812
nckuhsiang <chench@locust.csie.ncku.edu.tw>
2022-01-30 21:49:54 +0800

    Initial Commit

---
+++ Shared/DehMakeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DehMakeSwiftUIApp.swift
+//  Shared
+//
+//  Created by 陳家庠 on 2022/1/30.
+//
+
+import SwiftUI
+
+@main
+struct DehMakeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
